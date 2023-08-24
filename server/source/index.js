1d77c93a2dd1752e319352f4b3fa19c020f4dec5
KantoCanto <bruno.canto.neves@gmail.com>
2023-08-24 22:15:00 +0100

    created mongo atlas db connected to the db using mongoose created models folder to store models/schema created Deck.js as the first model that exports DeckModel DeckModel is currently being imported inside index.js

--- server/source/index.js
+++ server/source/index.js
@@ -1,8 +1,27 @@
-const express = require('express');
+//variable that holds the port number
+const PORT = 3000;
+
+//importing mongoose
+import mongoose from 'mongoose';
+
+//import the models
+import DeckModel from "./models/Deck.js";
+
+//importing express and creating app
+import express from 'express';
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello World')
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+//connection to the specific flashcardproject db
+//mongoose.connect() is a <promise>
+mongoose.connect(
+  "mongodb+srv://flascardproject:4uavobNvUDT1BkEH@cluster0.u5j5yv9.mongodb.net/?retryWrites=true&w=majority"
+).then(() =>{ 
+    console.log(`listening on port ${PORT}`)
+    app.listen(PORT);
 })
 
-app.listen(3000);
+
