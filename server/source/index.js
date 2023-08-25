6426680f9d285efc257080f44cd664fe64adf5fd
KantoCanto <bruno.canto.neves@gmail.com>
2023-08-25 15:16:12 +0100

    working on connecting db with user input

--- server/source/index.js
+++ server/source/index.js
@@ -8,12 +8,28 @@ const PORT = 3000;
 import mongoose from 'mongoose';
 
 //import the models
-import DeckModel from "./models/Deck.js";
+import Deck from "./models/Deck.js";
 
 //importing express and creating app
 import express from 'express';
 const app = express();
 
+//express.json() is a middleware function. here we tell express to use this.
+//everytime someone makes a request to our API, it's going to first run wtv this code is
+app.use(express.json());
+
+//routing for  /decks path
+app.post("/decks", async (req, res) => {
+  //here we want to be able to create a new deck model and persist that onto our database
+  const newDeck = new Deck({
+    title: req.body.title,
+  });
+  //this still doesn't communicate to our database, so we need to save it:
+  const createdDeck = await newDeck.save();
+  res.json(createdDeck);
+})
+
+//routing for requests on root path
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
