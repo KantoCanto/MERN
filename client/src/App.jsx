d809e70f6c58fdcf3a7502b29f4ca734d6913e30
KantoCanto <bruno.canto.neves@gmail.com>
2023-08-28 16:58:17 +0100

    Fixed the issue with the UI and the blank page. Created a form that can update its state to the user input and send that information to the backend API

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [title, setTitle] = useState("");
+
+  function handleCreateDeck(e){
+    e.preventDefault();
+    fetch("http://localhost:3000/decks",{
+      method: "POST",
+      body: JSON.stringify({
+        title,
+      }),
+    });
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" rel="noreferrer" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" rel="noreferrer" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="App">
+      <form onSubmit={handleCreateDeck}>
+        <label htmlFor="deck-title">Deck Title: </label>
+        <input id="deck-title" 
+          value = { title }
+          onChange={(e) =>{ 
+            setTitle(e.target.value)
+          }}/>
+        <button>Create Deck</button>
+      </form>
+    </div>
   );
 }
 
-export default App
+export default App;
