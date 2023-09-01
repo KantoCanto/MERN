05af179ffc74074855e51c0140368979363fea64
KantoCanto <bruno.canto.neves@gmail.com>
2023-09-01 11:57:22 +0100

    added react-router there is an app component and a deck component atm

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useEffect } from "react";
+import { Link } from "react-router-dom";
 import "./App.css";
 
 function App() {
@@ -47,19 +48,22 @@ function App() {
   return (
     <div className="App">
       <ul className="decks">
-        {decks.map((deck) => 
-            <li key={deck._id}>
-              <button onClick={() => handleDeleteDeck(deck._id)}>X</button>
-              {deck.title}</li>
-          )}
+        {decks.map((deck) => (
+          <li key={deck._id}>
+            <button onClick={() => handleDeleteDeck(deck._id)}>X</button>
+            <Link to={`decks/${deck._id}`}>{deck.title}</Link>
+          </li>
+        ))}
       </ul>
       <form onSubmit={handleCreateDeck}>
         <label htmlFor="deck-title">Deck Title: </label>
-        <input id="deck-title" 
-          value = { title }
-          onChange={(e) =>{ 
-            setTitle(e.target.value)
-          }}/>
+        <input
+          id="deck-title"
+          value={title}
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
+        />
         <button>Create Deck</button>
       </form>
     </div>
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import Deck from './Deck.jsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/decks/:deckId",
+    element: <Deck />,
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
+  <React.StrictMode> 
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
